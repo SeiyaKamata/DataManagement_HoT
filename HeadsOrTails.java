0f1019151eea1c209d1c13e0fff45e25818d7e9e
pl_kamata_seiya <kamata_seiya@plise.co.jp>
2020-07-30 10:44:51 +0900

    HoT: Initial revision

---
+++ HeadsOrTails.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+
+public class HeadsOrTails {
+    public static void main(String[] srgs) {
+        String[] s = {"Heads", "Tails"};
+        Random r = new Random();
+        int hc = 0, tc = 0;
+        System.out.println("Tossing a coin...");
+        for (int i = 1; i <= 3; i++) {
+            String value = s[r.nextInt(2)];
+            if (value.equals("Heads")) hc++;
+            else tc++;
+            System.out.println("Round " + i + ": " + value);
+        }
+
+        System.out.println("Heads: " + hc + ", Tails: " + tc);
+    }
+}
